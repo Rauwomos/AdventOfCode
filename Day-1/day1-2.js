5ba743e68bdc76588c76036052eb2931fd38421c
rw562 <rw562@bath.ac.uk>
2017-12-02 22:27:23 +0000

    Day 1 Works

---
+++ Day-1/day1-2.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+
+function parseInput(err, data) {
+    // If there was an error, stop
+    if(err)
+        throw err
+
+    // Read the file into an array of integers
+    const numArray = data.split('').map(i => parseInt(i))
+    var total = 0;
+
+    // Iterate over list and add double the value to the total if value half way the the array matches
+    for(var i=0; i < numArray.length/2; i++) {
+        if(numArray[i] == numArray[i+numArray.length/2])
+            total += numArray[i] * 2
+    }
+
+    // Output the result
+    console.log(total)
+}
+
+fs.readFile('./input.txt', 'utf-8', parseInput)
---
+++ Day-1/day1.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+
+function parseInput(err, data) {
+    // If there was an error, stop
+    if(err)
+        throw err
+
+    // Read the file into an array of integers
+    const numArray = data.split('').map(i => parseInt(i))
+    var total = 0;
+
+    // check the case for the first and last item in the list
+    if (numArray[0] == numArray[numArray.length-1])
+        total += numArray[0]
+    
+    // Iterate over list and add the value to the total if the neighbours match
+    for(var i=0; i<numArray.length-1;i++) {
+        if(numArray[i] == numArray[i+1])
+            total += numArray[i]
+    }
+
+    // Output the result
+    console.log(total)
+}
+
+fs.readFile('./input.txt', 'utf-8', parseInput)
