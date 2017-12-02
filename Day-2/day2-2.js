de685f99d21135f298c16a51d7b13a0f9a4e17ad
rw562 <rw562@bath.ac.uk>
2017-12-02 23:35:37 +0000

    Added comments Day 2

--- Day-2/day2-2.js
+++ Day-2/day2-2.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 
 function wholeDivisionInRow(row) {
+    // Not a fan of the brute force find method here
     for(var i=0;i<row.length; i++) {
             for(var j=0; j<row.length; j++) {
                 if(row[i]!=row[j] && row[i]%row[j] == 0) {
@@ -16,11 +17,15 @@ function parseInput(err, data) {
     if(err)
         throw err
 
+    // Parse input file into array of sorted arrays of numbers, one line per array
     var input2D = data.split('\n').map(row => row.split(',').map(val => parseInt(val)).sort((a,b) => a - b))
+    
+    // Calculate checksum
     var checksum = 0;
     for(i in input2D)
         checksum += wholeDivisionInRow(input2D[i])
 
+    // Output checksum
     console.log(checksum)
 
     
