4a3d8fbffadf0335c39e8ac86da27557d927a3c8
rw562 <rw562@bath.ac.uk>
2017-12-06 21:35:06 +0000

    Day 4 finished

--- Day-3/day3-2.js
+++ Day-3/day3-2.js
@@ -1,3 +1,15 @@
+function getFibN(number) {
+    return getFibN2(number, 1, 1, 3)
+}
+
+function getFibN2(number, prev, cur, count) {
+    next = cur + prev
+    if(next >= number) {
+        return count
+    }
+    return getFibN2(number, cur, next, count+1)
+}
+
 function dist(input) {
     sqrt = Math.sqrt(input)
     ceil = Math.ceil(sqrt)%2?Math.ceil(sqrt):Math.ceil(sqrt+1)
@@ -8,7 +20,9 @@ function dist(input) {
     console.log("Distance: " + (oneAxis + otherAxis))
 }
 
-dist(14)
-dist(20)
-dist(27)
-dist(368078)
+// dist(14)
+// dist(20)
+// dist(27)
+// dist(368078)
+
+console.log(getFibN(10))
